4a58acfc9ba9762582bf51771b29cac9d5e62a67
Satmeet <satmeet@ubuntu.ubuntu-domain>
2013-04-01 02:06:39 -0700

    removing backup folder

--- nodejs/tut_backup/scripts/find_diff.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-composite -compose difference $1 $2 out.png
-convert -threshold 10% -colorize 0/100/0 out.png out2.png
-rm out.png
-composite -compose plus $1 out2.png $3
-rm out2.png
